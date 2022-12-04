6a53dc46fe428e70693cfa65a472701951876c1c
Rami AMMAR <rammar@cegid.com>
2022-12-04 19:12:41 +0100

    part 1

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,10 +1,13 @@
+using eTickets;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
-
+startup.Configure(app, builder.Environment);
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
